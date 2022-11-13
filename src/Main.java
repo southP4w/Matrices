ec47f0ebfc9747fe8c0975f2e9397448125dbab7
southpaw <dangargiullo@gmail.com>
2022-11-13 16:37:01 -0500

    Fix toString() output for Matrix3D, add tests in Main.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,29 @@
 public class Main
 {
 	public static void main(String[] args) {
-		Matrix2D a = new Matrix2D(5, 5);
-		System.out.println("A\n" + a);
-		Matrix2D b = new Matrix2D(5, 5);
-		System.out.println("B\n" + b);
-		System.out.println("A + B");
-		System.out.println(Matrix2D.add(a, b));
-		System.out.println("A + A");
-		System.out.println(Matrix2D.add(a, a));
-		System.out.println("B + B");
-		System.out.println(Matrix2D.add(b, b));
-		Matrix2D c = new Matrix2D(5, 3);
-		System.out.println("C\n" + c);
-		Matrix2D d = new Matrix2D(3, 5);
-		System.out.println("D\n" + d);
-		System.out.println("C * D");
-		System.out.println(Matrix2D.multiply(c, d));
-		System.out.println("D * C");
-		System.out.println(Matrix2D.multiply(d, c));
+//		Matrix2D a = new Matrix2D(5, 5);
+//		System.out.println("A\n" + a);
+//		Matrix2D b = new Matrix2D(5, 5);
+//		System.out.println("B\n" + b);
+//		System.out.println("A + B");
+//		System.out.println(Matrix2D.add(a, b));
+//		System.out.println("A + A");
+//		System.out.println(Matrix2D.add(a, a));
+//		System.out.println("B + B");
+//		System.out.println(Matrix2D.add(b, b));
+//		Matrix2D c = new Matrix2D(5, 3);
+//		System.out.println("C\n" + c);
+//		Matrix2D d = new Matrix2D(3, 5);
+//		System.out.println("D\n" + d);
+//		System.out.println("C * D");
+//		System.out.println(Matrix2D.multiply(c, d));
+//		System.out.println("D * C");
+//		System.out.println(Matrix2D.multiply(d, c));
 
-
-
+		Matrix3D m1 = new Matrix3D(5, 6, 5);
+		Matrix3D m2 = new Matrix3D(6, 6, 6);
+		System.out.println("M1\n" + m1 + '\n');
+		System.out.println("M2\n" + m2);
+		System.out.println("M1 + M2\n" + Matrix3D.add(m1, m2));
 	}
 }
--- src/Matrix3D.java
+++ src/Matrix3D.java
@@ -2,71 +2,69 @@ import java.util.Random;
 
 public class Matrix3D
 {
-	private final int x, y, z;
+	private final int rows, columns, depth;
 	private final int[][][] cube;
 
 	public Matrix3D() {
-		x = y = z = 0;
-		cube = new int[x][y][z];
+		rows = columns = depth = 0;
+		cube = new int[rows][columns][depth];
 	}
 
-	public Matrix3D(int x, int y, int z) {
+	public Matrix3D(int rows, int columns, int depth) {
 		Random rand = new Random();
-		cube = new int[this.x = x][this.y = y][this.z = z];
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++) {
-				for (int k = 0; k < z; k++) {
-					cube[i][j][k] = rand.nextInt(1, 10);
+		cube = new int[this.rows = rows][this.columns = columns][this.depth = depth];
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < columns; col++) {
+				for (int dep = 0; dep < depth; dep++) {
+					cube[row][col][dep] = rand.nextInt(1, 10);
 				}
 			}
 		}
 	}
 
 	public static Matrix3D add(Matrix3D matrixA, Matrix3D matrixB) {
-		if (matrixA.x != matrixA.y || matrixA.x != matrixA.z || matrixA.y != matrixA.z) {
+		if (matrixA.rows != matrixA.depth || matrixA.columns != matrixA.depth) {
 			System.out.println("UNDEFINED: Matrix A is not a cube!");
 			return null;
 		}
-		if (matrixB.x != matrixB.y || matrixB.x != matrixB.z || matrixB.y != matrixB.z) {
+		if (matrixB.rows != matrixB.depth || matrixB.columns != matrixB.depth) {
 			System.out.println("UNDEFINED: Matrix B is not a cube!");
 			return null;
 		}
-		if (matrixA.x != matrixB.x || matrixA.y != matrixB.y || matrixA.z != matrixB.z) {
+		if (matrixA.rows != matrixB.rows) {
 			System.out.println("UNDEFINED: Matrix A and Matrix B are not equally-sized cubic matrices!");
 			return null;
 		}
-		Matrix3D sum = new Matrix3D(matrixA.x, matrixA.y, matrixA.z);
+		Matrix3D sum = new Matrix3D(matrixA.rows, matrixA.columns, matrixA.depth);
 		zero(sum);
-		for (int i = 0; i < matrixA.x; i++)
-			for (int j = 0; j < matrixA.y; j++)
-				for (int k = 0; k < matrixA.z; k++)
-					sum.cube[i][j][k] = matrixA.cube[i][j][k] + matrixB.cube[i][j][k];
+		for (int row = 0; row < matrixA.rows; row++)
+			for (int col = 0; col < matrixA.columns; col++)
+				for (int dep = 0; dep < matrixA.depth; dep++)
+					sum.cube[row][col][dep] = matrixA.cube[row][col][dep] + matrixB.cube[row][col][dep];
 
 		return sum;
 	}
 
 	public static void zero(Matrix3D matrix) {
-		for (int i = 0; i < matrix.x; i++) {
-			for (int j = 0; j < matrix.y; j++) {
-				for (int k = 0; k < matrix.z; k++) {
+		for (int i = 0; i < matrix.rows; i++) {
+			for (int j = 0; j < matrix.columns; j++) {
+				for (int k = 0; k < matrix.depth; k++) {
 					matrix.cube[i][j][k] = 0;
 				}
 			}
 		}
 	}
 
-	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++) {
-				for (int k = 0; k < z; k++) {
-					sb.append(cube[i][j][k]).append(' ');
+		StringBuilder sb = new StringBuilder().append("{\n");
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < columns; col++) {
+				for (int dep = 0; dep < depth; dep++) {
+					sb.append("[").append(row).append(", ").append(col).append(", ").append(dep).append("] = ").append(cube[row][col][dep]).append('\n');
 				}
-				sb.append("|\n");
 			}
-			sb.append('\n');
 		}
+		sb.append('}');
 
 		return sb.toString();
 	}
